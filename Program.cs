91bfbebb66ecfff14838dd12444d889b4c8c5c8f
MaxDac <massimiliano.dacunzo@hotmail.com>
2022-11-23 14:27:11 +0000

    Added Logging and Configuration

--- Program.cs
+++ Program.cs
@@ -1,9 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddOptions<Settings>()
+	.BindConfiguration(nameof(Settings));
+
+builder.Services.AddLogging(options =>
+{
+	options.AddConsole();
+	options.AddDebug();
+});
 
 var app = builder.Build();
 
@@ -21,17 +33,26 @@ var summaries = new[]
 	"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
 };
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/weatherforecast", (IOptionsSnapshot<Settings> settings, ILoggerFactory logFactory) =>
 {
-	var forecast = Enumerable.Range(1, 5).Select(index =>
-		new WeatherForecast
-		(
-			DateTime.Now.AddDays(index),
-			Random.Shared.Next(-20, 55),
-			summaries[Random.Shared.Next(summaries.Length)]
-		))
+	var logger = logFactory.CreateLogger("WeatherForecast");
+	logger.LogInformation("Getting weather forecast in {TemperatureUnit} unit", settings.Value.TemperatureUnit.ToString());
+	return Enumerable.Range(1, 5)
+	  .Select(index =>
+			new WeatherForecast
+			(
+				DateTime.Now.AddDays(index),
+				Random.Shared.Next(-20, 55),
+				summaries[Random.Shared.Next(summaries.Length)]
+			))
+		.Select(m => settings switch 
+		{
+			{Value: {TemperatureUnit: TemperatureUnit.Fahrenheit}} =>
+				new WeatherForecastDto(m.Date, m.TemperatureC, m.Summary),
+			_ =>
+				new WeatherForecastDto(m.Date, m.TemperatureF, m.Summary),
+		})
 		.ToArray();
-	return forecast;
 })
 .WithName("GetWeatherForecast");
 
@@ -40,4 +61,17 @@ app.Run();
 internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
 {
 	public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
+
+internal record struct WeatherForecastDto(DateTime Date, int Temperature, string? Summary);
+
+internal enum TemperatureUnit
+{
+	Celsius,
+	Fahrenheit
+}
+
+internal class Settings
+{
+	public TemperatureUnit TemperatureUnit { get; set; }
 }
